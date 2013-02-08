11fb3161882f3888f1ab7e71fa9c379f8d370681
Matt M <matthew.alexander.maher@gmail.com>
2013-02-07 20:33:52 -0500

    withPHP

--- Folder1/Index.php
+++ Folder1/Index.php
@@ -1,3 +1,22 @@
-()php;
+<?php
 
-echo 'Hello World';
+  $database = new mysqli('instance38441.db.xeround.com','Matthew', 'buhloon', '6435');
+  
+  if($database->connect_error > 0){
+      echo 'Database error'  . $database->connect_error; 
+  }else{
+      echo 'Connected!';
+  }
+ 
+ $query = 'SELECT FROM * persons';
+ 
+ $result = $database->query($query);
+ 
+ if(!$result){
+     echo 'Database query error!' . $database->error;
+ }else{
+     while($row = $result->fetch_assoc()){
+         echo 'Name: ' . $row['name'];
+     }
+ }
+ 
