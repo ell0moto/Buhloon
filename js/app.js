bd9c69dfcef072326893a6763e32ad1007d65083
Roger Qiu <cmcdragonkai@gmail.com>
2013-03-27 20:03:11 +1100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,25 @@
 
 var app = angular.module('App', [
 	'Controllers',
-
-
+	'Filters',
+	'Services',
+	'Directives',
+	'ngResource',
+	'ngCookies'
 ]);
 
+//Define all the page level controllers (Application Logic)
 angular.module('Controllers', [
-	'Home.Controllers',
+	'Home.Controllers'
+]);
+//Define all shared services (Interaction with Backend)
+angular.module('Services', [
+]);
+//Define all shared directives (UI Logic)
+angular.module('Directives', [
+]);
+//Define all shared filters (UI Filtering)
+angular.module('Filters', [
 ]);
 
 //router
@@ -15,27 +28,47 @@ app.config(
 	[
 		'$routeProvider',
 		'$locationProvider',
-		function ($routeProvider, $locationProvider) {
-
-			//HTML5 mode URLS I believe it removes /#/ hashbang
-			$locationProvider.html5Mode (true).hashPrefix('!');
-
+		function($routeProvider, $locationProvider) {
+			
+			//HTML5 Mode URLs
+			$locationProvider.html5Mode(true).hashPrefix('!');
+			
 			//Routing
 			$routeProvider
 				.when(
-					'/', 
+					'/',
 					{
-						templateUrl:'home_index.html',
-						controller:'HomeIndexCtrl'
+						templateUrl: 'home_index.html',
+						controller: 'HomeIndexCtrl'
 					}
 				)
-
 				.otherwise(
 					{
 						redirectTo: '/'
 					}
 				);
+			
 		}
-
 	]
 );
+
+app.run([
+	'$rootScope',
+	'$cookies',
+	'$http',
+	function($rootScope, $cookies, $http){
+	
+		//XSRF INTEGRATION
+		$rootScope.$watch(
+			function(){
+				return $cookies[serverVars.csrfCookieName];
+			},
+			function(){
+				$http.defaults.headers.common['X-XSRF-TOKEN'] = $cookies[serverVars.csrfCookieName];
+			}
+		);
+		
+		//XHR ERROR HANDLER
+		
+	}
+]);
