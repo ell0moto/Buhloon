973b8c6bf10b184d27842abd4cfbe0262b00e21e
Roger Qiu <cmcdragonkai@gmail.com>
2013-03-27 20:06:26 +1100

    Update footer_partial.php

--- application/views/partials/footer_partial.php
+++ application/views/partials/footer_partial.php
@@ -15,39 +15,6 @@
         </div>
     </footer>
 
-        <!-- client side templates -->
-        <? Template::partial('home/home_index'); ?>
-
-
-        <!-- Pass in PHP variables to Javascript -->
-        <script>
-            var serverVars = {
-                baseUrl: '<?= base_url() ?>',
-                csrfCookieName: '<?= $this->config->item('cookie_prefix') . $this->config->item('csrf_cookie_name') ?>'
-            };
-        </script>
-        
-        <!-- Java script. "ajax.googleapis" is where google hosts jQuery files as it is faster that way -->
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-        <script src="<?= base_url() ?>/js/vendor/bootstrap.min.js"></script>
-
-        <script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js"></script>
-        <script>window.angular || document.write('<script src="js/vendor/angular.min.js"><\/script>')</script>
-        
-        <script src="js/vendor/angular-resource.min.js"></script>
-        <script src="js/vendor/angular-cookies.min.js"></script>
-        <script src="js/app.js"></script>
-        <script src="js/controllers/Home.Controllers.js"></script>
-
-
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
-
     <!-- Rewards Modal -->
     <div id="rewards" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-header">
@@ -187,6 +154,38 @@
         </div>
 
     </div> <!-- End of model -->
+
+
+        <!-- client side templates -->
+        <? Template::partial('home/home_index'); ?>
+
+        <!-- Pass in PHP variables to Javascript -->
+        <script>
+            var serverVars = {
+                baseUrl: '<?= base_url() ?>',
+                csrfCookieName: '<?= $this->config->item('cookie_prefix') . $this->config->item('csrf_cookie_name') ?>'
+            };
+        </script>
+        
+        <!-- Java script. "ajax.googleapis" is where google hosts jQuery files as it is faster that way -->
+    	<!-- Vendor Javascripts -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+		<script src="js/vendor/bootstrap.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.0.5/angular.min.js"></script>
+		<script>window.angular || document.write('<script src="js/vendor/angular.min.js"><\/script>')</script>
+		<script src="js/vendor/angular-resource.min.js"></script>
+		<script src="js/vendor/angular-cookies.min.js"></script>
+        
+        <script src="js/app.js"></script>
+        <script src="js/controllers/Home.Controllers.js"></script>
+
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
 
     </body>
 </html>
